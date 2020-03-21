0f67ed00078d1627b91ba269c3ad5fe16ed763c3
root <root@aio.home.io>
2020-03-21 09:05:02 -0400

    added run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app.config.from_pyfile('settings.py')
 
 @app.route('/')
 def main():
-    return f'Hello, world from hostname: { app.config.get("HOSTNAME") }' 
+    return f'Hello, world from hostname: { app.config.get("HOSTNAME") }<br>Running in namespace: { app.config.get("NAMESPACE")' 
 
 if __name__ == '__main__':  # Script executed directly?
     print("Hello World! Built with a Docker file.")
--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
 from os import environ
 
 HOSTNAME = environ.get('HOSTNAME')
+NAMESPACE = environ.get('NAMESPACE')
